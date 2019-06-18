f2b92e0faa106386fa43c194054aa380a54c0abe
Anirudh Samudrala <anirudh.enigma@gmail.com>
2019-06-17 21:41:00 -0700

    updating images

--- lib/widgets/LandingPage.dart
+++ lib/widgets/LandingPage.dart
@@ -20,11 +20,13 @@ class LandingPage extends StatelessWidget {
         /*    appBar: AppBar(
           title: Text(title, style: TextStyle(color: Colors.black38)),
         ),*/
-        appBar:
-        AppBar(
+        appBar: AppBar(
           elevation: 0.1,
           backgroundColor: Colors.blueGrey,
-          title: Text(title,style: TextStyle(fontFamily: 'NTR', color:Colors.white), ),
+          title: Text(
+            title,
+            style: TextStyle(fontFamily: 'NTR', color: Colors.white),
+          ),
           actions: <Widget>[
             IconButton(
               icon: Icon(Icons.list),
@@ -32,11 +34,57 @@ class LandingPage extends StatelessWidget {
             )
           ],
         ),
-        body: new Container(
+        body:Stack(
+          children: <Widget>[
+/*            Container(
+                padding: EdgeInsets.only(left: 10.0),
+                decoration: new BoxDecoration(
+                  color: Color.fromRGBO(20, 16, 33, .7),
+                  image: new DecorationImage(
+                    fit: BoxFit.cover,
+                    colorFilter: new ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                    image: AssetImage('assets/images/sandhya.png')))),*/
+            new Container(
+              child: new Center(
+                child: new FutureBuilder(
+                    future: DefaultAssetBundle.of(context)
+                        .loadString('assets/data/' + this.mantraFile),
+                    builder: (context, snapshot) {
+                      var menus = json.decode(snapshot.data.toString());
+
+                      return GridView.builder(
+                        physics: const NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: menus.length,
+                        gridDelegate: new SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 3,
+                        ),
+                        itemBuilder: (BuildContext context, int index) {
+                          return new GestureDetector(
+                              child: new CircleIcon(
+                                  iconImage: menus[index]['iconImage'],
+                                  iconText: menus[index]['iconText']),
+                              onTap: () => Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (BuildContext context) => ListPage(
+                                        mantras:
+                                        MenuItem.fromJson(menus[index]).mantras,
+                                        title: menus[index]['iconText']),
+                                  )));
+                        },
+                      );
+                    }),
+              ),
+            ),
+          ],
+        ),
+
+/*        new Container(
           child: new Center(
             child: new FutureBuilder(
                 future: DefaultAssetBundle.of(context)
-                    .loadString('assets/data/'+this.mantraFile),
+                    .loadString('assets/data/' + this.mantraFile),
                 builder: (context, snapshot) {
                   var menus = json.decode(snapshot.data.toString());
 
@@ -55,14 +103,16 @@ class LandingPage extends StatelessWidget {
                           onTap: () => Navigator.push(
                               context,
                               MaterialPageRoute(
-                                builder: (BuildContext context) =>
-                                    ListPage(mantras: MenuItem.fromJson(menus[index]).mantras,title:menus[index]['iconText']),
+                                builder: (BuildContext context) => ListPage(
+                                    mantras:
+                                        MenuItem.fromJson(menus[index]).mantras,
+                                    title: menus[index]['iconText']),
                               )));
                     },
                   );
                 }),
           ),
-        ),
+        ),*/
       ),
     );
   }
